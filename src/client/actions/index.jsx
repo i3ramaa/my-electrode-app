9383eb5f9776cf48bcf97aba59c7deef3e7381a3
ramadhevi <ramadhevi@i3software.in>
2017-02-08 13:32:14 +0530

    first commit

---
+++ src/client/actions/index.jsx
@@ -0,0 +1,11 @@
+export const toggleCheck = () => {
+  return {type: "TOGGLE_CHECK"};
+};
+
+export const incNumber = () => {
+  return {type: "INC_NUMBER"};
+};
+
+export const decNumber = () => {
+  return {type: "DEC_NUMBER"};
+};
